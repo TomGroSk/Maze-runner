a731f7757e8246c64cc8fc7dc0cce6c77961d730
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2020-04-22 00:48:44 +0200

    Added samples sprites and map scrolling

--- Maze-Runner-Client/Game.py
+++ Maze-Runner-Client/Game.py
@@ -2,18 +2,14 @@ import sys
 import pygame
 from pygame.locals import *
 
-from Position import Position2d
-
+from Player import Player
+from Wall import Wall
+from Road import Road
 
 class Game:
 
     windowSize = width, height = 800, 600
-    playerPosition = Position2d(400, 300)
-
-    # test only
-    player = pygame.image.load("img/hello.png")
-    playerrect = player.get_rect()
-    # end test only
+    cameraX, cameraY = -400, -300
 
     def __init__(self):
         pygame.init()
@@ -21,18 +17,46 @@ class Game:
 
         self.screen = pygame.display.set_mode(self.windowSize)
 
+        self.players = []
+        self.walls = []
+        self.roads = []
+
     def run(self):
+        self.loadInitDataFromServer()
+
         while 1:
             self.executeGameLogic()
 
+    def loadInitDataFromServer(self):  # test
+        # load init data from server
+            # player id
+            # other player data
+            # map
+        mazeArray = [[False, False, False, False], [True, False, True, False], [False, True, True, False], [True, True, False, True]]
+        self.prepareMap(mazeArray)
+
+        self.mainPlayer = Player(1, 0, 0)
+
+    def prepareMap(self, mazeArray):
+        x, y = 0, 0
+        for row in mazeArray:
+            for cell in row:
+                if cell:
+                    self.walls.append(Wall(y, x))
+                self.roads.append(Road(y, x))
+                y += 128
+            x += 128
+            y = 0
+
     def executeGameLogic(self):
-        # event handling
+        # load data from server
+
         self.handleKeyboard()
-        # print("x: " + str(self.playerPosition.x) + " y:" + str(self.playerPosition.y))
 
         # collisions
 
-        # drawing
+        # send data to server
+
         self.draw()
 
     def handleKeyboard(self):
@@ -41,22 +65,40 @@ class Game:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == K_w:
-                    self.playerPosition.y -= 10
+                    self.mainPlayer.rect.y -= 10
+                    self.cameraY -= 10
                 if event.key == K_s:
-                    self.playerPosition.y += 10
+                    self.mainPlayer.rect.y += 10
+                    self.cameraY += 10
                 if event.key == K_a:
-                    self.playerPosition.x -= 10
+                    self.mainPlayer.rect.x -= 10
+                    self.cameraX -= 10
                 if event.key == K_d:
-                    self.playerPosition.x += 10
+                    self.mainPlayer.rect.x += 10
+                    self.cameraX += 10
 
     def draw(self):
         self.screen.fill((0, 0, 0))  # clean screen
 
-        # test only
-        self.screen.blit(self.player, (self.playerPosition.x, self.playerPosition.y))
-        # test only
+        # roads
+        for road in self.roads:
+            self.screen.blit(road.sprite, self.remap(road.rect))
+
+        # walls
+        for wall in self.walls:
+            self.screen.blit(wall.sprite, self.remap(wall.rect))
+
+        # other players
+        for player in self.players:
+            self.screen.blit(player.sprite, self.remap(player.rect))
+
+        # main player
+        self.screen.blit(self.mainPlayer.sprite, self.remap(self.mainPlayer.rect))
 
         pygame.display.flip()  # display on screen
+
+    def remap(self, rect):
+        return rect.x - self.cameraX, rect.y - self.cameraY
 
 
 game = Game()
---
+++ Maze-Runner-Client/Player.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Player:
+    id = -1
+
+    def __init__(self, id, x, y):
+        self.id = id
+
+        self.sprite = pygame.image.load("img/win.png") #("img/player" + str(id) + ".png")
+        self.rect = self.sprite.get_rect()
+
+        self.rect.x = x
+        self.rect.y = y
+
+    def setPosition(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
